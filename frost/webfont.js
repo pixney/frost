f9923098edc2747d573300d0d525b22dd1cd76d0
William <william@pixney.com>
2022-05-21 15:05:41 +0200

    Initial Commit

---
+++ frost/webfont.js
@@ -0,0 +1,24 @@
+r
+const plugin = require('tailwindcss/plugin')
+
+
+const base = plugin(function ({ addComponents, addBase, theme }) {
+
+    addBase({
+        '@font-face': [
+            {
+                fontFamily: 'Inter-Thin',
+                fontWeight: 400,
+                src: "url('/webfont/Inter-Thin.ttf') format('ttf')"
+            },
+            {
+                fontFamily: 'Inter-Black',
+                fontWeight: 700,
+                src: "url('/webfont/Inter-Black.ttf') format('ttf')"
+            },
+            
+        ],
+        
+    })
+})
+module.exports = base
